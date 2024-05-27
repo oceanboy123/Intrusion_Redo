c00f3cc0027a368553b19162efb03c00663b58c3
oceanboy123 <eddg@dal.ca>
2024-05-27 20:27:11 -0300

    Selecting target data. Datetimes need to be changed to intergers

--- iding_script.py
+++ iding_script.py
@@ -14,3 +14,11 @@ import pandas as pd
 
 raw_bbmp_data = 'bbmp_aggregated_profiles.csv'
 BBMP_data = pd.read_csv(raw_bbmp_data)
+
+target_variables = ['time_string','pressure','salinity','temperature']
+target_data = BBMP_data.loc[:,target_variables]
+
+dates_type_datetime = pd.to_datetime(target_data.iloc[:,0])
+target_data['time_string'] = dates_type_datetime
+
+
