c1d098525e170fc02c873f17c032e6757f0b4549
oceanboy123 <eddg@dal.ca>
2024-05-30 12:52:37 -0300

    Erasing duplicated or/and empty files

--- intrusion_identification.py
+++ intrusion_identification.py
@@ -10,3 +10,19 @@ def intrusion_identification(temp_values, salt_values, dates, coefficients):
     Both_intrusion_dates = [value for value in temperature_intrusion_dates.values.tolist() if value in salinity_intrusion_dates.values.tolist()]
     
     return Both_intrusion_dates
+
+
+#plt.imshow(temperature_m_inter_12.to_numpy(), cmap = 'jet')
+#plt.colorbar()
+#plt.show()
+
+#    Found out there are timestamps that are different and therefore
+#    have multiple measuerements for the same day. Making some profiles
+#    have a lot of repited depths. 
+#
+#sizess = []
+#for key, values in normalized_data.items():
+#    sizess.append(len(pd.DataFrame(values)))
+#
+#not_good_indices = [index for index, value in enumerate(sizess) if value != 276]
+#wrong_data_length = pd.DataFrame(normalized_data.keys()).iloc[not_good_indices]
