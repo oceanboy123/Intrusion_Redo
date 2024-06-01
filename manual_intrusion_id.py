b63c99d367ad050b720216b1c6ddc838f08b2bd5
oceanboy123 <eddg@dal.ca>
2024-06-01 14:57:22 -0300

    N/A

--- manual_intrusion_id.py
+++ manual_intrusion_id.py
@@ -1,4 +1,5 @@
 import global_functions as gf
+import matplotlib.pyplot as plt
 
 file_name = 'BBMP_salected_data.pkl'
 
@@ -8,6 +9,11 @@ yearly_profiles = gf.separate_yearly_profiles(selected_data)
 
 range_1 = [0,10]
 range_2 = [30.5,31.5]
-gf.plot_year_profiles(selected_data, yearly_profiles, 
+fig = gf.plot_year_profiles(selected_data, yearly_profiles, 
                       2023,[range_1, range_2])
 
+cid_click = fig['Figure'].canvas.mpl_connect('buttom_press_event', gf.onclick)
+
+cid_key = fig['Figure'].canvas.mpl_connect('key_press_event', gf.onkey)
+
+plt.show()
