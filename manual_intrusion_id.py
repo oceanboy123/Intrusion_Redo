c7b6898e02d96146685c2049d045f7c3020c224c
oceanboy123 <eddg@dal.ca>
2024-06-01 15:29:06 -0300

    Testing selection of intrusions fo multple years

--- manual_intrusion_id.py
+++ manual_intrusion_id.py
@@ -1,5 +1,6 @@
 import global_functions as gf
 import matplotlib.pyplot as plt
+from datetime import datetime
 
 file_name = 'BBMP_salected_data.pkl'
 
@@ -18,24 +19,32 @@ def onclick(event):
         event.inaxes.plot(x, y, 'ro')
         event.canvas.draw()
 
-
-
 def onkey(event):
     if event.key == ' ':
         plt.close(event.canvas.figure)
 
-
-
 def get_points():
     return points
 
+
 range_1 = [0,10]
 range_2 = [30.5,31.5]
-fig = gf.plot_year_profiles(selected_data, yearly_profiles, 
-                      2023,[range_1, range_2])
+points_year = {}
 
-cid_click = fig['Figure'].canvas.mpl_connect('button_press_event', onclick)
+timestamp = selected_data['sample_timestamps']
+year_list = list(set([datetime.fromtimestamp(stamp).year for stamp in timestamp]))
 
-cid_key = fig['Figure'].canvas.mpl_connect('key_press_event', onkey)
+for yr in year_list:
+    fig = gf.plot_year_profiles(selected_data, yearly_profiles, 
+                        yr,[range_1, range_2])
 
-plt.show()
+    cid_click = fig['Figure'].canvas.mpl_connect('button_press_event', onclick)
+
+    cid_key = fig['Figure'].canvas.mpl_connect('key_press_event', onkey)
+
+    points_year[str(yr)] = get_points()
+
+    plt.show()
+
+points_name = 'manual_intrusions_'+ str(year_list[0]) + str(year_list[-1])
+gf.save_joblib(points_name, points_year)
