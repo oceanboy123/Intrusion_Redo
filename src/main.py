2150799b1e0d8b532bd70d66bcf4f5d84d34eff8
oceanboy123 <eddg@dal.ca>
2024-06-12 22:00:42 -0300

    na

--- src/main.py
+++ src/main.py
@@ -9,7 +9,6 @@ file_name = input("Enter the file name for intrusion identification (include .pk
 if int(file_name) == 1:
     file_name = 'BBMP_salected_data0.pkl'
 
-
 lin = "-"*6+' '
 print(lin+'Importing Data')
 selected_data = gf.import_joblib(file_name)
