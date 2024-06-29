d25aacf49f0399824f1fe36b9e7b9b941afd0043
oceanboy123 <eddg@dal.ca>
2024-06-29 07:45:43 -0300

    Completed chnages for data acquisition.

--- src/get_ftp_BBMP.sh
+++ src/get_ftp_BBMP.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-
 # Define variables
 FTP_SERVER="ftp.dfo-mpo.gc.ca"
 FTP_USER="anonymous"
 FTP_PASS="guest"
 REMOTE_DIR="/BIOWebMaster/BBMP/CSV"
 FILE_NAME="bbmp_aggregated_profiles.csv"
-
 ftp -inv ftp.dfo-mpo.gc.ca <<EOF
 user $FTP_USER 
 pass $FTP_PASS
@@ -14,5 +12,5 @@ cd $REMOTE_DIR
 get $FILE_NAME
 bye
 EOF
-
+mv bbmp_aggregated_profiles.csv ../DATA/RAW
 echo "Download completed. File saved as $FILE_NAME"
