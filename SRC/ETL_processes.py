dea995cb047c30efd1bffb47c4dd16b50de3d70b
oceanboy123 <eddg@dal.ca>
2024-07-23 10:38:14 -0300

    Finished docstrings and format for ETL_processes

--- SRC/ETL_processes.py
+++ SRC/ETL_processes.py
@@ -1,4 +1,4 @@
-# Importing relevant modules
+# Imports
 import joblib
 import os
 import time
@@ -44,10 +44,23 @@ def get_command_line_args():
 
 
 class Intrusion_ETL:
-
+    """Creates an Object representing a specific ETL strategy
+    for the BBMP aggregated csv file, including keeping 
+    record of metadata"""
+    
+    # Internal Variables
+    # Based on current schema and setup
     original_datename = 'time_string'
     original_pressure_name = 'pressure'
     groupby_datename = 'Timestamp'
+
+    # Diectory paths and file names
+    output_file_name = 'BBMP_salected_data0.pkl'
+    output_file_path = '../data/PROCESSED/' + output_file_name
+    metadata_csv = 'metadata_processing.csv'
+    metadata_csv_path = '../data/PROCESSED/' + metadata_csv
+    
+    # Transformation names per variable
     transformation_names = [
             '_interpolated_axis0',
             '_interpolated_axis10',
@@ -55,17 +68,18 @@ class Intrusion_ETL:
             '_avg_diff1_inter10',
             '_avgmid_diff1_inter10'
         ]
-    output_file_name = 'BBMP_salected_data0.pkl'
-    output_file_path = '../data/PROCESSED/' + output_file_name
-    metadata_csv = 'metadata_processing.csv'
-    metadata_csv_path = '../data/PROCESSED/' + metadata_csv
+    
 
     def __init__(self, file_name: str, variables_target: list[str], deep_depth: int, mid_depth: list[int], date_format: str) -> None:
+        
+        # Defining ETL strategy characteristics
         self.original_file_name = file_name
         self.target_variables = variables_target
         self.deep_depth = deep_depth
         self.mid_depth = mid_depth
         self.date_format = date_format
+
+        # Key data storages within ETL process (including metadata)
         self.target_data = {}
         self.nested_groups = {}
         self.unique_depths = []
@@ -76,6 +90,7 @@ class Intrusion_ETL:
         self.transform_data = {}
         self.metadata = {}
 
+        # Recording ETL strategy characteristics as metadata
         stat_info = os.stat(self.original_file_name)
         self.metadata['Date_created'] = time.ctime(stat_info.st_birthtime)
         self.metadata['Target_variables'] = str(self.target_variables)
@@ -84,6 +99,9 @@ class Intrusion_ETL:
 
 
     def get_target_data(self)-> None:
+        """Read data from csv file and records target variables
+        as a pandas DataFrame"""
+
         print('Reading CSV file')
         bbmp_data = pd.read_csv(self.original_file_name)
         
@@ -95,10 +113,13 @@ class Intrusion_ETL:
 
         dates_type_int:list[int] = [days.timestamp() for days in dates_type_datetime]
         target_data[self.groupby_datename] = dates_type_int
-        self.target_data : dict[str, list[str|int]] = target_data
+        
+        # DataFrame of target variables
+        self.target_data = target_data
 
 
     def group_data(self) -> None:
+        """Group data by timestamps, aka separate data by profile"""
 
         print('Updating target data and grouping by day')
         grouped_by_date = self.target_data.groupby(self.groupby_datename)
@@ -107,16 +128,23 @@ class Intrusion_ETL:
         for group_name, group_data in grouped_by_date:
             nested_groups[group_name] = group_data
 
+        # Separated profiles
         self.nested_groups : dict = nested_groups
         self.metadata['Profile_count'] = [len(self.nested_groups)]
 
         unique_depths = list(set(list(self.target_data.iloc[:, 1])))
         unique_depths.sort()
 
+        # Unique depths for all profiles
         self.unique_depths : list = unique_depths
 
 
     def normalize_depth_from_list(self, upress: list, data_frame):
+        """Use list of unique depths from group_data() to normalize
+        the depths available in a profile by creating new rows
+        where expected depths are missing and filling with NaN
+        values"""
+
         for p in upress:
             if p not in data_frame.iloc[:, 1].values:
 
@@ -167,16 +195,23 @@ class Intrusion_ETL:
 
             data[key] = data_frame
 
+        # Normalized data
         self.normalized_data : dict = data
 
         normalized_depths = data[list(data.keys())[0]][self.original_pressure_name].tolist()
         normalized_dates = list(data.keys())
 
-        self.normalized_dates : list= normalized_dates
+        # Final list of unique depths
+        self.normalized_dates : list = normalized_dates
+        # Final list of unique dates
         self.normalized_depth : list = normalized_depths
 
 
-    def separate_target_variables(self, string_name: str): 
+    def separate_target_variables(self, string_name: str):
+        """Creates an NParray for one of the target variables where
+        the y-axis represents depth and the x-axis represents
+        date"""
+
         print('Creating Target Variable Matrices')
         all_columns = np.transpose([values[string_name] for key, values in self.normalized_data.items()])
 
@@ -184,32 +219,46 @@ class Intrusion_ETL:
     
 
     def get_variable_matrices(self) -> None:
+        """Creates NParrays for all target variables"""
+
+        # NParray for all target variables
         self.variables_matrices = [self.separate_target_variables(names) for names in self.target_variables[2:]]
 
 
     def data_transformations(self) -> None:
+        """Performs trasnformations of values. Incluiding 2D
+        interpolation of profile data to fill NaNs, mainly the 
+        ones added for depth normalization
         
-        matrix_list = self.variables_matrices
+        In addition, it also calculates depth averages based
+        on the deep_depth and mid_depth variables, and calculates
+        the n-th order discrete difference along the date axis
+        for those depth averages"""
 
         print('Interpolating Data')
 
         transform_data = {}
         count = 0
-        for matrix in matrix_list:
+        for matrix in self.variables_matrices:
             pandas_matrix = pd.DataFrame(matrix)
+
+            # 2D interpolation
             matrix_interpolated_axis0 = pandas_matrix.interpolate(axis=0).replace(0, np.nan)
             matrix_interpolated_axis10 = matrix_interpolated_axis0.interpolate(axis=1).replace(0, np.nan)
+
+            # Discrete difference calculation
             matrix_diff = pd.DataFrame(np.diff(matrix_interpolated_axis10, axis=1)).replace(0, np.nan)
 
+            # Calculate depth averages
             normal_depths = np.array(self.normalized_depth)
             rows_bellow60 = list(np.where(normal_depths > self.deep_depth)[0])
+
             rows_over35 = list(np.where(normal_depths < self.mid_depth[1])[0])
             rows_under20 = list(np.where(normal_depths > self.mid_depth[0])[0])
-            print(rows_under20)
             rows_btw20_35 = sorted(list(set(rows_over35+rows_under20)))
-            print(rows_btw20_35)
-            matrix_avg_below = matrix_diff.iloc[rows_bellow60, :].mean(axis=0)
-            matrix_avg_btw = matrix_diff.iloc[rows_btw20_35, :].mean(axis=0)
+
+            matrix_avg_below = matrix_diff.iloc[rows_bellow60, :].mean(axis=0) # Deep depth average
+            matrix_avg_btw = matrix_diff.iloc[rows_btw20_35, :].mean(axis=0) # Mid depth average
 
             t_names = self.transformation_names
 
@@ -221,12 +270,16 @@ class Intrusion_ETL:
             
             count += 1
 
+        # Transformed data ready for loading
         self.transform_data : dict = transform_data
 
 
     def conform_schema(self) -> None:
+        """Load data into predifined schema used in Intrusion_analysis.py.
+        NOTE: if this the dict keys chnage, make sure to change them in
+        the analysis python script"""
+        
         transformed_data = self.transform_data
-
         self.output_data = {
         'sample_diff_midrow_temp': transformed_data['temperature_avgmid_diff1_inter10'],
         'sample_diff_row_temp': transformed_data['temperature_avg_diff1_inter10'],
@@ -246,6 +299,8 @@ class Intrusion_ETL:
         
 
     def record_output_metadata(self) -> None:
+        """Record metadata and save file for analysis"""
+
         self.metadata['Output_dataset_path'] = self.output_file_path
         meta_processing = pd.DataFrame(self.metadata)
 
@@ -253,16 +308,19 @@ class Intrusion_ETL:
             read = csv.reader(file)
             row_count = sum(1 for _ in read)
 
+        # Record metadata
         if row_count == 0:
             meta_processing.to_csv(self.metadata_csv_path, mode='a', header=True, index=False)
         else:
             meta_processing.to_csv(self.metadata_csv_path, mode='a', header=False, index=False)
 
+        # Save .pkl file for analysis
         joblib.dump(self.output_data, self.output_file_path)
         print(f'Saved as {self.output_file_name}')
 
 
 def main():
+    # Get command line arguments
     raw_name, deep_depth, mid_depth, date_format = get_command_line_args()
     raw_bbmp_data = '../data/RAW/' + raw_name
 
@@ -273,16 +331,23 @@ def main():
                         'temperature',
                         'oxygen']
 
+    # Initializing ETL_Intrusion object
     bbmp = Intrusion_ETL(raw_bbmp_data, target_variables, deep_depth, mid_depth, date_format)
+    
+    # Data Manipulation
     bbmp.get_target_data()
     bbmp.group_data()
-
     print(bbmp.nested_groups[list(bbmp.nested_groups.keys())[0]].head())
-
     bbmp.normalize_length_data()
     bbmp.get_variable_matrices()
+
+    # Data Transformation
     bbmp.data_transformations()
+    
+    # Schema
     bbmp.conform_schema()
+
+    # Data Loading and Metadata Recording
     bbmp.record_output_metadata()
 
     return bbmp
