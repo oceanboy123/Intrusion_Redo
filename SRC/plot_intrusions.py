3478dcd9726df66cd51beb473694ac4c88db07bd
oceanboy123 <eddg@dal.ca>
2024-08-31 15:07:17 -0300

    plot_intrusions functional with multiple years but for datetimes

--- SRC/plot_intrusions.py
+++ SRC/plot_intrusions.py
@@ -1,5 +1,16 @@
 from Intrusion_identification.manual_identification import manual_identification
 from misc.request_arguments.request_info_analysis import RequestInfo_Analysis
+from misc.request_arguments.get_cmdline_args import get_command_line_args
+from datetime import datetime
+
+
+# varsin = {
+#             'file_name': 'BBMP_salected_data0.pkl',
+#             'i_year': 2021,
+#             'f_year': 2021
+#             }
+    
+# raw_name, i_year, f_year = get_command_line_args(varsin)
 
 file_name= 'BBMP_salected_data0.pkl'
 intrusion_type= 'NORMAL'
@@ -22,7 +33,12 @@ request = RequestInfo_Analysis(
                             manual_input = manual_input
                             )
 
+# d1 = datetime(2021,6,1)
+# d2 = datetime(2023,6,1)
+# datetimes = [d1,d2]
+
 XX = manual_identification('NORMAL','OFF')
 XX.fill_request_info(request.dates)
 yearly_profiles = XX.separate_yearly_profiles(request)
-fig = XX.plot_year_profiles(yearly_profiles, 2021, request, yr2= 2024)
+fig = XX.plot_year_profiles(yearly_profiles, 2021, request, yr2=2024)
+
