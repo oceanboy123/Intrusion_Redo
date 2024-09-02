5e906ae03974dc3b438b6c1a89313e342e43af5f
oceanboy123 <eddg@dal.ca>
2024-09-02 11:15:28 -0300

    Adding to previous commit, ETL_methods are yet to be optimized

--- SRC/ETL_processes/ETL_method.py
+++ SRC/ETL_processes/ETL_method.py
@@ -5,12 +5,6 @@ from dataclasses import dataclass
 @dataclass
 class ETL_method(ABC):
     data_info : RequestInfo
-
-    # @abstractmethod
-    # def __str__(self):
-    #     x = self.metadata
-    #     string_separated = [f'{i}: {j}' for i,j in x.items()]
-    #     return f'Request Info: {string_separated}'
     
     @abstractmethod
     def GenerateLog(self) -> None:
