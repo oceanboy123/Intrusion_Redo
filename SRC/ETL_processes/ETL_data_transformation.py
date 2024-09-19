256c1ebd04c8b6ed0f858b5570dc47d744680b90
oceanboy123 <eddg@dal.ca>
2024-09-18 22:07:28 -0300

    data_transformation Clean - 1

--- SRC/ETL_processes/ETL_data_transformation.py
+++ SRC/ETL_processes/ETL_data_transformation.py
@@ -34,67 +34,84 @@ class data_transformation(ETL_method):
 
     def interpolation_2D(self, pandas_matrix: DataFrame) -> list[DataFrame]:
         """
-        Performs interpolation on the target variable matrices.Incluiding 2D
-        interpolation of profile data to fill NaNs, mainly the ones added for 
-        depth normalization. 
+        Performs interpolation on the target variable matrices, including 2D 
+        interpolation of profile data to fill NaNs, mainly those added during 
+        depth normalization.
         
-        With no row interpolation, only column interpolation.
+        Only column interpolation is performed (axis=0).
         """
         # Column interpolation
-        interpolated0_data = pandas_matrix.interpolate(axis=0).replace(0, np.nan)
+        interpolated0_data = pandas_matrix.interpolate(axis=0, 
+                                                       limit_direction='both'
+                                                       ).replace(0, np.nan)
 
         # Calculate changes in variables from profile to profile
-        inter0_diff = pd.DataFrame(np.diff(interpolated0_data, axis=0))
+        inter0_diff = interpolated0_data.diff(axis=0)
         zero_diff = inter0_diff == 0
-        mask_diff = np.zeros_like(interpolated0_data, dtype=bool)
-        mask_diff[1:] = zero_diff
 
-        interpolated0_data[mask_diff] = np.nan
+        mask_diff = zero_diff.shift(-1, fill_value=False)
 
-        # Row interpolation
+        interpolated0_data = interpolated0_data.mask(mask_diff)
+
+        # Row interpolation (Not performed)
         interpolated_data = interpolated0_data
-        inter_diff = np.diff(interpolated_data, axis=1)
-        interpolated_diff_data = pd.DataFrame(inter_diff).replace(0, np.nan)
+
+        # Difference along axis=1 (rows)
+        inter_diff = interpolated_data.diff(axis=1)
+        interpolated_diff_data = inter_diff.replace(0, np.nan)
 
         return [interpolated_data, interpolated_diff_data, interpolated0_data]
 
 
-    def depth_averages(self, interpolation: list[DataFrame]) -> list[DataFrame]:
+    def depth_averages(self, interpolation: list[DataFrame]) -> list[pd.Series]:
         """
         Identifies the depths and calculates the depth-averages for both mid
-        and bottom time-series. For the actual values, and the n-th order 
+        and bottom time-series for the actual values and the n-th order 
         discrete difference along the date axis
+
+        Returns:
+        - List[pd.Series]: List containing:
+            - avg_below_diff: Mean of differences at deep depths.
+            - avg_mid_diff: Mean of differences at mid depths.
+            - avg_below_value: Mean of values at deep depths.
+            - avg_mid_value: Mean of values at mid depths.
         """
-        deep = self.data_info.deep_depth
-        normal_depths = np.array(self.data_normalization.normalized_depth)
-        rows_bellow60 = list(np.where(normal_depths > deep)[0])
+        deep_depth = self.data_info.deep_depth
+        normalized_depths = np.array(self.data_normalization.normalized_depth)
 
-        mid = self.data_info.mid_depth
-        rows_over35 = list(np.where(normal_depths < mid[1])[0])
-        rows_under20 = list(np.where(normal_depths > mid[0])[0])
-        rows_btw20_35 = sorted(list(set(rows_over35+rows_under20)))
+        indices_below_deep = np.where(normalized_depths > deep_depth)[0]
 
-        matrix = interpolation[0]
-        matrix_diff = interpolation[1]
-        
-        # Bottom depths
-        avg_below_value = matrix.iloc[rows_bellow60, :].mean(axis=0)
-        avg_below = matrix_diff.iloc[rows_bellow60, :].mean(axis=0)
+        mid_depth_range = self.data_info.mid_depth
+        indices_mid_depth = np.where(
+            (normalized_depths > mid_depth_range[0]) & 
+            (normalized_depths < mid_depth_range[1])
+        )[0]
 
-        # Mid depths
-        avg_btw_value = matrix.iloc[rows_btw20_35, :].mean(axis=0)
-        avg_btw = matrix_diff.iloc[rows_btw20_35, :].mean(axis=0)
+        interpolated_data = interpolation[0]
+        interpolated_diff_data = interpolation[1]
 
-        return [avg_below, avg_btw, avg_below_value, avg_btw_value]
+        # Bottom depths averages
+        avg_below_value = interpolated_data.iloc[indices_below_deep, 
+                                                 :].mean(axis=0)
+        avg_below_diff = interpolated_diff_data.iloc[indices_below_deep, 
+                                                     :].mean(axis=0)
+
+        # Mid depths averages
+        avg_mid_value = interpolated_data.iloc[indices_mid_depth, 
+                                               :].mean(axis=0)
+        avg_mid_diff = interpolated_diff_data.iloc[indices_mid_depth, 
+                                                   :].mean(axis=0)
+
+        return [avg_below_diff, avg_mid_diff, avg_below_value, avg_mid_value]
 
 
     def run(self) -> None:
         """
-        Steps: interpolation_2D -> depth_averages
+        Executes the data transformation process:
+        -   For each variable matrix, performs 2D interpolation and calculates 
+            depth averages.
         """
-        transform_data = {}
-        count = 2
-        for matrix in self.timedepth_space.variables_matrices:
+        for var_name, matrix in self.timedepth_space.variables_matrices.items():
             pandas_matrix = pd.DataFrame(matrix)
 
             # 2D interpolation
@@ -104,22 +121,25 @@ class data_transformation(ETL_method):
             depth_avg = self.depth_averages(interpolated)
 
             t_names = self.transformation_names
-            v_names = self.data_info.target_variables
 
-            transform_data[v_names[count]+t_names[0]] = interpolated[2]
-            transform_data[v_names[count]+t_names[1]] = interpolated[0]
-            transform_data[v_names[count]+t_names[2]] = interpolated[1]
-            transform_data[v_names[count]+t_names[3]] = depth_avg[0]
-            transform_data[v_names[count]+t_names[4]] = depth_avg[1]
-            
-            count += 1
-
-        self.transform_data: dict = transform_data
+            self.transform_data[var_name + t_names[0]] = interpolated[2]
+            self.transform_data[var_name + t_names[1]] = interpolated[0]
+            self.transform_data[var_name + t_names[2]] = interpolated[1]
+            self.transform_data[var_name + t_names[3]] = depth_avg[0] 
+            self.transform_data[var_name + t_names[4]] = depth_avg[1]
         
 
     def GenerateLog(self, logger: Logger) -> None:
         """
-        Log transform_eg.head(20)
+        Logs a sample of the transformed data.
         """
-        transform_eg = self.transform_data[list(self.transform_data.keys())[3]]
-        logger.info(f'\nDeep Data: \n{transform_eg.head(20)}')
+        if self.transform_data:
+            sample_key = list(self.transform_data.keys())[3]
+            transform_sample = self.transform_data[sample_key].head(20)
+            logger.info(
+                f'\nDeep Data Sample ({sample_key}):\n{transform_sample}'
+                )
+        else:
+            logger.info(
+                'No transformed data to display.'
+                )
