00de08ed2bd958d3fc5c5825b1c4f2b248ca835d
oceanboy123 <eddg@dal.ca>
2024-09-18 23:58:53 -0300

    Addition of metaclass function

--- SRC/ETL_processes/config.py
+++ SRC/ETL_processes/config.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 
 from pandas import DataFrame
+from abc import ABC, abstractmethod
 from dataclasses import dataclass, field
 from typing import Dict, Any, List
 from logging import Logger
 
 from .ETL_method import ETL_method
+from misc.other.metadata_handling import DocInheritMeta
