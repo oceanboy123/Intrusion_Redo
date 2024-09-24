8e4e48c6ccf5a6b114ef2539467d965ddace1c9d
oceanboy123 <eddg@dal.ca>
2024-09-24 20:08:25 -0300

    Created __init__ for the request_arguments module

---
+++ SRC/misc/request_arguments/__init__.py
@@ -0,0 +1,4 @@
+from .get_cmdline_args import get_command_line_args
+from .request_info_analysis import RequestInfo_Analysis
+from .request_info_ETL import (RequestInfo_ETL, 
+                               RequestInfo)
