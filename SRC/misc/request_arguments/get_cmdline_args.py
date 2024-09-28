ef6a76597ab4bcfc827f609e51026c3de18d3e6a
oceanboy123 <eddg@dal.ca>
2024-09-28 17:12:35 -0300

    Chnaged to return a Dictionary

--- SRC/misc/request_arguments/get_cmdline_args.py
+++ SRC/misc/request_arguments/get_cmdline_args.py
@@ -1,7 +1,9 @@
 import argparse
-from typing import Dict, Union, Tuple
+from typing import Dict, Union, Any
 
-def get_command_line_args(varsin: Dict[str, Union[str, int]]) -> Tuple:
+def get_command_line_args(
+        varsin: Dict[str, Union[str, int, float]]
+    ) -> Dict[str, Any]:
     """
     Gets arguments from command-line. Uses varsin to create the defaults for
     each input argument.
@@ -9,22 +11,23 @@ def get_command_line_args(varsin: Dict[str, Union[str, int]]) -> Tuple:
     ----------------Inputs
     varsin :   Default values for each argument
     """
-    parser = argparse.ArgumentParser(description=
-                                     'Command-line arguments parser')
+    parser = argparse.ArgumentParser(
+        description='Command-line arguments parser'
+    )
 
     # Command line arguments
     for arg, default_value in varsin.items():
         # Determine type based on default value
         arg_type = type(default_value)
-        parser.add_argument('--' + arg, type=arg_type, default=default_value,
-                            help=f'Default: {default_value}')
+        parser.add_argument(
+            '--' + arg, 
+            type=arg_type, 
+            default=default_value, 
+            help=f'Default: {default_value}'
+        )
 
     # Parse arguments
     args = parser.parse_args()
-    output = []
-
-    for arg in varsin.keys():
-        value = getattr(args, arg)
-        output.append(value)
+    output = {arg: getattr(args, arg) for arg in varsin.keys()}
             
-    return tuple(output)
+    return output
